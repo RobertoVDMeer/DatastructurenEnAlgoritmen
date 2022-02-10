ac8bee47ed14c258c715be530dfed3cd21cc6303
Roberto van der Meer <robertovdmeer@gmail.com>
2022-02-10 06:55:04 +0100

    Back to the drawing board, the code was based around the idea that higher numbers on the left and lower on the right would meet in the middle, e.g. the left index (high numbers), could check right until the total was too high then move to next, until the indexes meet. However, I hadan't forseen the situation where the right (low) side had negative numbers, so it is useful to still go through the whole array. But, by now my code was so convoluted, I might as well start over.

--- src/Les1.java
+++ src/Les1.java
@@ -5,7 +5,7 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Les1 {
-    public static String filePath = "./src/real-01.txt";
+   public static String filePath = "./src/sample-01.txt";
    public static void main(String args[]) throws FileNotFoundException {
       Scanner scanner = new Scanner(new File(filePath));
        int aantal = scanner.nextInt();
@@ -26,57 +26,18 @@ public class Les1 {
    }
 
     public static int pairsTotalNum(int[] nums, int total) {
-      Arrays.sort(nums);
-        System.out.println(MessageFormat.format("original array: {0}",
-                Arrays.toString(nums)));
-      flipArray(nums);
-        System.out.println(MessageFormat.format("sorted array: {0}",
-                Arrays.toString(nums)));
-      System.out.println(MessageFormat.format("flipped array: {0}",
+      System.out.println(MessageFormat.format("original array: {0}",
               Arrays.toString(nums)));
+      Arrays.sort(nums);
 
       int lowI = nums.length - 1;
       int highNum = nums[0];
       int lowNum = nums[lowI];
       int totalPairs = 0;
 
-      for(int highI = 0; highI < nums.length && highI < lowI; highI++, lowI--){
-          if(highI !=0 && lowI != nums.length -1) {
-              // check for doubles
-              if (highNum == nums[highI]) { lowI++; continue; }
-              if (lowNum == nums[lowI]) { highI--; continue; }
-          }
+      return totalPairs;
 
-          highNum = nums[highI];
-          lowNum = nums[lowI];
-
-//          System.out.println(MessageFormat.format("{0} + {1} = {2}",
-//                  highNum, lowNum, highNum + lowNum));
-          if (lowNum > highNum) break;
-
-          if(highNum + lowNum == total){
-              System.out.println(MessageFormat.format("{0} + {1} = {2}",
-                      highNum, lowNum, total));
-              totalPairs++;
-          }
-      }
-
-       return totalPairs;
     }
 
-    public static int gcd(int p, int q) {
-        if (q == 0) return p;
-        int r = p % q;
-        return gcd(q, r);
-    }
-    public static void flipArray(int[] a){
-        int N = a.length;
-        for (int i = 0; i < N / 2; i++) {
-            int leftNum = a[i];
-            int rightIndex = N - 1 - i;
-            a[i] = a[rightIndex];
-            a[rightIndex] = leftNum;
-        }
-    }
 }
 
