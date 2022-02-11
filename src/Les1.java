03ae743d4cc08a4ee33f57db1c355fdb6acba132
Roberto van der Meer <robertovdmeer@gmail.com>
2022-02-11 11:15:08 +0100

    It's Alive! I created a monster, refusing to just copy the solution code, but getting lost in spaghetti code. I'll post it anyway, have a go at the homework, then ditch it and use the solution code anyway!

--- src/Les1.java
+++ src/Les1.java
@@ -5,87 +5,73 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Les1 {
-   public static String filePath = "./src/sample-01.txt";
-   public static void main(String args[]) throws FileNotFoundException {
-      Scanner scanner = new Scanner(new File(filePath));
-       int aantal = scanner.nextInt();
-       int targetSum = scanner.nextInt();
+  public static String filePath = "./src/real-01.txt";
 
-       int[] nums = new int[aantal];
-       int i = 0;
+  public static void main(String args[]) throws FileNotFoundException {
+    Scanner scanner = new Scanner(new File(filePath));
+    int aantal = scanner.nextInt();
+    int targetSum = scanner.nextInt();
 
-       while (scanner.hasNext()) {
-           if (scanner.hasNextInt()) {
-               nums[i] = scanner.nextInt();
-               i++;
-           }
-       }
-      System.out.println(MessageFormat.format(
-              "total unique pairs found for sum {0}: {1}"
-              ,targetSum , pairsTotalNum(nums, targetSum)));
-   }
+    int[] nums = new int[aantal];
+    int i = 0;
 
-    public static int pairsTotalNum(int[] nums, int targetSum) {
-      System.out.println(MessageFormat.format("original array: {0}"
-              , Arrays.toString(nums)));
-      Arrays.sort(nums);
-        System.out.println(MessageFormat.format("sorted array: {0}"
-                , Arrays.toString(nums)));
+    while (scanner.hasNext()) {
+      if (scanner.hasNextInt()) {
+        nums[i] = scanner.nextInt();
+        i++;
+      }
+    }
 
-      int lowI = 0;
-      int highI = nums.length - 1;
-      int highNum = nums[highI];
-      int lowNum = nums[lowI];
-      int totalPairs = 0;
+    System.out.println(MessageFormat.format(
+        "total unique pairs found for sum {0}: {1}"
+        , targetSum, pairsTotalNum(nums, targetSum)));
+  }
 
-      // TODO Sanity arroy of 2, 1, equal numbers
-      while (lowNum < highNum) {
+  public static int pairsTotalNum(int[] nums, int targetSum) {
+    Arrays.sort(nums);
 
-          // check for doubles and skip
-          if(lowI!=0 && lowNum == nums[lowI - 1]) {
-              System.out.println("low double");
-              lowI++;
-              lowNum = nums[lowI];
-               continue;
-          }
-          if(highI != nums.length -1 && highNum == nums[highI + 1]) {
-              System.out.println("high double");
-              highI--;
-              highNum = nums[highI];
-              continue;
-          }
+    int lowI = 0;
+    int highI = nums.length - 1;
+    int highNum = nums[highI];
+    int lowNum = nums[lowI];
+    int totalPairs = 0;
 
-          int sum = nums[lowI] + nums[highI];
-          System.out.println(MessageFormat.format("{0} + {1} = {2}",
-                  nums[lowI], nums[highI], sum));
-        if(sum == targetSum) {
-          totalPairs++;
-          lowI++;
-          highI = nums.length -1;
-          System.out.println(MessageFormat.format("{0} + {1} = {2}"
-                  ,lowNum, highNum, sum));
-        } else if (sum < targetSum) {
-            System.out.println("sum lower than target");
-            highI--;
-        } else {
-            lowI++;
-            highI = nums.length -1;
-        }
-          System.out.println(MessageFormat.format("old low: {0} old high {1}"
-                  ,lowNum, highNum));
-          lowNum = nums[lowI];
-          highNum = nums[highI];
-          System.out.println(MessageFormat.format("new low: {0} new high {1}"
-                  ,lowNum, highNum));
-          if(lowI + 1 == highI) {
-              System.out.println("about to run into pointer");
-              lowI++;
-              highI = nums.length -1;
-          }
+    while (lowI < highI) {
+      // check for doubles and skip
+      if (lowI != 0 && lowNum == nums[lowI + 1]) {
+        lowI++;
+        lowNum = nums[lowI];
+        continue;
+      }
+      if (highI != nums.length - 1 && highNum == nums[highI - 1]) {
+        highI--;
+        highNum = nums[highI];
+        continue;
       }
 
-      return totalPairs;
+      int sum = nums[lowI] + nums[highI];
+      if (sum == targetSum) {
+        totalPairs++;
+        lowI++;
+        highI = nums.length - 1;
+        System.out.println(MessageFormat.format("Found a match: " +
+            "{0} + {1} = {2}", lowNum, highNum, sum));
+      } else if (sum > targetSum) {
+        highI--;
+      } else {
+        lowI++;
+      }
+      lowNum = nums[lowI];
+      highNum = nums[highI];
+      if (lowI == highI && lowI != nums.length - 1) {
+        lowI++;
+        highI = nums.length - 1;
+        lowNum = nums[lowI];
+        highNum = nums[highI];
+      }
     }
+    return totalPairs;
+  }
 
 }
 
