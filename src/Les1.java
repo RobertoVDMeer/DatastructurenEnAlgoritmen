29a54ee6f30f231afbe87a9baa87b72eb62b1277
Roberto van der Meer <robertovdmeer@gmail.com>
2022-02-14 07:58:33 +0100

    - Moved the data to its own dir - Added some comments for clarification - Refactored the match message to handle a sum of 3 - Removed the solution provided by Arjen - Refactored findTriplets vars to match the project

--- src/Les1.java
+++ src/Les1.java
@@ -5,8 +5,8 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Les1 {
-//  public static String filePath = "./src/sample-01.txt";
-  public static String filePath = "./src/real-01.txt";
+//  public static String filePath = "./src/data/sample-01.txt";
+  public static String filePath = "./src/data/real-01.txt";
 
   public static void main(String args[]) throws FileNotFoundException {
     Scanner scanner = new Scanner(new File(filePath));
@@ -23,25 +23,23 @@ public class Les1 {
       }
     }
 
-//    System.out.println(MessageFormat.format(
-//        "total unique pairs found for sum {0}: {1}"
-//        , targetSum, findPairs(nums, targetSum, 0)));
-
-//    nums = new int[] {1, -1, 0, 2, 3};
-//    targetSum = 2;
     System.out.println(MessageFormat.format(
-        "total unique triplets found for sum {0}: {1}"
-        , targetSum, findTriplets(nums, targetSum)));
+        "Total unique {0} found for sum {1}: {2}"
+        , "triplets",  targetSum, findTriplets(nums, targetSum)));
   }
 
-  public static int findPairs(int[] nums, int targetSum, int startIndex) {
+  // startI is used by findTriplets, basically saying which number it is
+  // and ignore anything before / lower / left of that
+  public static int findPairs(int[] nums, int targetSum, int startI) {
     Arrays.sort(nums);
-
-    int lowI = startIndex;
+    int lowI = startI;
     int highI = nums.length - 1;
+    int totalPairs = 0;
+
+    // TODO On refactoring, realized these highNum and lowNum aren't needed,
+    // TODO will leave it though, as I plan to rename the method into frankenCode anyway!
     int highNum = nums[highI];
     int lowNum = nums[lowI];
-    int totalPairs = 0;
 
     while (lowI < highI) {
       // check for doubles and skip
@@ -57,19 +55,32 @@ public class Les1 {
       }
 
       int sum = nums[lowI] + nums[highI];
+
       if (sum == targetSum) {
+        // found match, move to next low number and reset the high number
         totalPairs++;
         lowI++;
         highI = nums.length - 1;
-//        System.out.println(MessageFormat.format("Found a match: " +
-//            "{0} + {1} = {2}", lowNum, highNum, sum));
+
+        if (startI == 0) {
+          System.out.println(MessageFormat.format("Found a match: " +
+            "{0} + {1} = {2}", lowNum, highNum, sum));
+        } else {
+          System.out.println(MessageFormat.format("Found a match: " +
+              "{0} + {1} + {2} = {3}",nums[startI - 1], lowNum, highNum
+              , sum + nums[startI -1]));
+        }
       } else if (sum > targetSum) {
         highI--;
       } else {
         lowI++;
       }
+
       lowNum = nums[lowI];
       highNum = nums[highI];
+
+      // if the pointers meet and the left (low) isn't at the end, move it and
+      // reset the high pointer
       if (lowI == highI && lowI != nums.length - 1) {
         lowI++;
         highI = nums.length - 1;
@@ -80,37 +91,15 @@ public class Les1 {
     return totalPairs;
   }
 
-  public static int solutionQuizOne(int[] arr, int targetSum) {
-    Arrays.sort(arr);
-    int left = 0;
-    int right = arr.length - 1;
-
-    int toTarget = 0;
-
-    while (left < right) {
-      int currentSum = arr[left] + arr[right];
-      if (currentSum == targetSum) {
-        toTarget += 1;// res.add(Arrays.asList(arr[left], arr[right]));
-        System.out.println(arr[left] + " " + arr[right]);
-        while (left < right && arr[left] == arr[++left]) ;
-        while (left < right && arr[right] == arr[--right]) ;
-      } else if (currentSum > targetSum) {
-        right--;
-      } else {
-        left++;
-      }
-    }
-    return toTarget;
-  }
-
-  public static int findTriplets(int[] arr, int targetSum) {
+  public static int findTriplets(int[] nums, int targetSum) {
+    Arrays.sort(nums);
     int matches = 0;
-    Arrays.sort(arr);
-    int curr = 0;
+    int i = 0;
 
-    while (arr[curr] < targetSum && curr != arr.length - 3) {
-      matches += findPairs(arr, targetSum - arr[curr], curr +1);
-      curr++;
+    while (nums[i] < targetSum && i != nums.length - 2) {
+      // just find pairs for the sum minus the current number
+      matches += findPairs(nums, targetSum - nums[i], i + 1);
+      i++;
     }
 
     return matches;
