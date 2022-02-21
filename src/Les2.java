80bf401d3eeb977253e4cc02f722d2446a4272c6
Roberto van der Meer <robertovdmeer@gmail.com>
2022-02-21 15:49:17 +0100

    - Implemented the Parens balance check

--- src/Les2.java
+++ src/Les2.java
@@ -1,5 +1,40 @@
+import java.text.MessageFormat;
+import java.util.*;
+
 public class Les2 {
   public static void main(String[] args) {
-    System.out.println("blurg");
+    checkParens("(()(()[])[])[[(())()]])[[[((([])[[]])]]]");
+//    checkParens("([()][");
+  }
+
+  public static void checkParens(String input) {
+    // opener's value is same as key (we add it to the stack)
+    final Map<Integer, Integer> openers = Map.of((int)'(', (int)'(', (int)'[', (int)'[');
+    // the closers's value is the corresponding opener to check against
+    final Map<Integer, Integer> closers = Map.of((int)')', (int)'(', (int)']', (int)'[');
+
+    Stack<Integer> parensStack = new Stack<Integer>();
+
+    input.chars().forEach(i -> {
+          if (openers.get(i) != null) {
+            parensStack.add(i);
+          } else if (closers.get(i) != null){
+            int prev = parensStack.isEmpty() ? 0 : parensStack.pop();
+            if (closers.get(i) != prev) {
+              System.out.println(MessageFormat.format("unbalanced {0} found, prev = {1} "
+                  , (char) i, (char)prev));
+            }
+          }
+        }
+    );
+
+    if(!parensStack.isEmpty()) {
+      Iterator<Integer> i = parensStack.iterator();
+      while(i.hasNext()){
+
+        System.out.println(MessageFormat.format("this paren is never closed: {0}"
+            , Character.toString(i.next())));
+      }
+    }
   }
 }
