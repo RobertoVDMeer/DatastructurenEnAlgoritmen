2d39116f63d4c11a1f5c9c674d42aa7c16ff5dcd
Roberto van der Meer <robertovdmeer@gmail.com>
2022-02-22 06:56:23 +0100

    - Refactor to not use reflection loop, as I need to return a bool for the next assigment, which cannot be reached from within the loop
    - Prepare for homework week 2

--- src/Les2.java
+++ src/Les2.java
@@ -3,38 +3,52 @@ import java.util.*;
 
 public class Les2 {
   public static void main(String[] args) {
-    checkParens("(()(()[])[])[[(())()]])[[[((([])[[]])]]]");
+//    checkParens("(()(()[])[])[[(())()]])[[[((([])[[]])]]]");
 //    checkParens("([()][");
+    calculate("(2 + 2)");
+    calculate("(2 + (1 + 1))");
+    calculate("((8 / 4) + (1 + 1))");
+    calculate("(1 + ((2 + 3) * (4 * 5))");
   }
 
-  public static void checkParens(String input) {
+  public static void calculate(String input) {
+    System.out.println(checkParens(input));
+  }
+
+  public static boolean checkParens(String input) {
     // opener's value is same as key (we add it to the stack)
-    final Map<Integer, Integer> openers = Map.of((int)'(', (int)'(', (int)'[', (int)'[');
+    final Map<Integer, Integer> openers = Map.of((int) '(', (int) '(', (int) '[', (int) '[');
     // the closers's value is the corresponding opener to check against
-    final Map<Integer, Integer> closers = Map.of((int)')', (int)'(', (int)']', (int)'[');
+    final Map<Integer, Integer> closers = Map.of((int) ')', (int) '(', (int) ']', (int) '[');
+
+    boolean balanced = true;
 
     Stack<Integer> parensStack = new Stack<Integer>();
 
-    input.chars().forEach(i -> {
-          if (openers.get(i) != null) {
-            parensStack.add(i);
-          } else if (closers.get(i) != null){
-            int prev = parensStack.isEmpty() ? 0 : parensStack.pop();
-            if (closers.get(i) != prev) {
-              System.out.println(MessageFormat.format("unbalanced {0} found, prev = {1} "
-                  , (char) i, (char)prev));
-            }
-          }
+    Iterator<Integer> c = input.chars().iterator();
+    while (c.hasNext()) {
+      int car = c.next();
+      if (openers.get(car) != null) {
+        parensStack.add(car);
+      }
+      else if (closers.get(car) != null) {
+        int prev = parensStack.isEmpty() ? 0 : parensStack.pop();
+        if (closers.get(car) != prev) {
+          System.out.println(MessageFormat.format("unbalanced {0} found, prev = {1} "
+              , (char) car, (char) prev));
+          balanced = false;
         }
-    );
+      }
+    }
 
-    if(!parensStack.isEmpty()) {
+    if (!parensStack.isEmpty()) {
       Iterator<Integer> i = parensStack.iterator();
-      while(i.hasNext()){
-
+      while (i.hasNext()) {
         System.out.println(MessageFormat.format("this paren is never closed: {0}"
             , Character.toString(i.next())));
       }
+      balanced = false;
     }
+    return balanced;
   }
 }
